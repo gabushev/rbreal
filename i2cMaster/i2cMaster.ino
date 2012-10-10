429940e478bdb0b369e66233bb116adfc1ea8b6a
gabushev <gabushev@gmail.com>
2012-10-10 09:54:54 +0400

    i2c master send

--- i2cMaster/i2cMaster.ino
+++ i2cMaster/i2cMaster.ino
@@ -1,9 +1,12 @@
-#include <Wire.h>  //подключаем библиотеку 
+#include <Wire.h> 
+#include <IRremote.h>
 #define ADDR 128  //адрес нужного нам Slave
 #define LED_PIN 13
 #define BTN_PIN 9
 
 int val = 0;
+char fromSlave = ' ';
+IRsend irsend;
 
 void setup()
 {
@@ -11,16 +14,31 @@ void setup()
   pinMode(BTN_PIN, INPUT);
   digitalWrite(LED_PIN, LOW);    // turn the LED off by making the voltage LOW  
   Wire.begin(); //включить i2c
+  Serial.begin(9600);
 }
 void loop() {
   val = digitalRead(BTN_PIN);
   digitalWrite(LED_PIN, val);
   if (val == HIGH)
   {
-    Wire.beginTransmission(ADDR); //начало передачи, с адресом нужного нам Slave 
-    Wire.write("Test, communication."); //отправляем данные для выбраного Slave 
-    Wire.endTransmission();
-    delay(1000);
+    for (int i = 0; i < 3; i++)
+    {
+      irsend.sendSony(0xa90, 12); // Sony TV power code
+      delay(100);
+    }
+  }
+  trySlave();
+  delay(300);
+}
+
+void trySlave(){
+  Wire.requestFrom(ADDR,1, true);
+    while(Wire.available()){
+      fromSlave = Wire.read();
+    }
+  if (fromSlave == '!'){
+    Serial.println("yes");
+    fromSlave = ' ';
   }
 }
 
