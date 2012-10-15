7897f03551a25631aefb0f2fa84157ea246e0b0e
nivax <9782201@gmail.com>
2012-10-15 08:48:37 +0400

    Мухи от котлет

--- i2cMaster/i2cMaster.ino
+++ i2cMaster/i2cMaster.ino
@@ -1,12 +1,9 @@
-#include <Wire.h> 
-#include <IRremote.h>
+#include <Wire.h>  //подключаем библиотеку 
 #define ADDR 128  //адрес нужного нам Slave
 #define LED_PIN 13
 #define BTN_PIN 9
 
 int val = 0;
-char fromSlave = ' ';
-IRsend irsend;
 
 void setup()
 {
@@ -14,31 +11,16 @@ void setup()
   pinMode(BTN_PIN, INPUT);
   digitalWrite(LED_PIN, LOW);    // turn the LED off by making the voltage LOW  
   Wire.begin(); //включить i2c
-  Serial.begin(9600);
 }
 void loop() {
   val = digitalRead(BTN_PIN);
   digitalWrite(LED_PIN, val);
   if (val == HIGH)
   {
-    for (int i = 0; i < 3; i++)
-    {
-      irsend.sendSony(0xa90, 12); // Sony TV power code
-      delay(100);
-    }
-  }
-  trySlave();
-  delay(300);
-}
-
-void trySlave(){
-  Wire.requestFrom(ADDR,1, true);
-    while(Wire.available()){
-      fromSlave = Wire.read();
-    }
-  if (fromSlave == '!'){
-    Serial.println("yes");
-    fromSlave = ' ';
+    Wire.beginTransmission(ADDR); //начало передачи, с адресом нужного нам Slave 
+    Wire.write("Test, communication."); //отправляем данные для выбраного Slave 
+    Wire.endTransmission();
+    delay(1000);
   }
 }
 
